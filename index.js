f7dee06f0430f121a5cc0c61045228d08f6613ad
nhidayahj <hidayah.joha@gmail.com>
2021-06-11 06:32:19 +0000

    todos app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 const hbs = require("hbs");
 const wax = require("wax-on")
 
@@ -13,6 +14,9 @@ const mongoUrl = process.env.MONGO_URL
 // initialise express
 let app = express();
 
+app.use(express.json());
+app.use(cors());
+
 app.set("view engine", "hbs")
 app.use(express.static('public'))
 
@@ -26,32 +30,60 @@ app.use(express.urlencoded({
 
 // SETUP ROUTES
 async function main() {
-    let db = await MongoUtil.connect(mongoUrl, "tgc11_recipe_app")
+    let db = await MongoUtil.connect(mongoUrl, "nhj-todo-app")
 
-    // view all ingredient list from mongodb
-    app.get("/ingredients", async (req,res) => {
-        let all_ingredients = await db.collection('ingredients')
+    app.get('/profiles', async (req,res) => {
+        let allProfiles = await db.collection('profiles')
             .find({})
             .toArray()
 
-            res.render('ingredients/all', {
-                'ingredientList':all_ingredients
+            res.send(allProfiles);
+    })
+
+    // register new user profiles 
+    app.post("/profile/register", async (req,res) => {
+        try {
+            let newProfile = await db.collection('profiles')
+                .insertOne({
+                    'username':req.body.username,
+                    'userID':req.body.userID,
+                    'date_joined':req.body.date_joined,
+                    'friends':req.body.friendsList
+                    
+                })
+                console.log(newProfile.ops[0])
+                res.send(newProfile);
+        } catch (e) {
+            res.status(500);
+            res.send({
+                'error':"Error posting new ingredient"
             })
+            console.log(e);
+        }
     })
 
-    // get the /create page from hbs folder
-    app.get("/ingredients/create", async (req,res) => {
-        res.render("ingredients/create");
+    // see user's todo cards 
+    app.get("/:profile/todos", async (req,res) => {
+        // let userProfileID = req.params.profile;
+        
+        let userProfile = await db.collection('profiles')
+            .findOne({
+                username:req.params.profile
+            })
+        console.log("user profile: ",userProfile)
+        
+        let userTodo = await db.collection('todoCards')
+            .find({
+                userID:userProfile._id
+            }).toArray()
+        // console.log("found todos: ",userTodo)
+        res.status(200);
+        res.send(userTodo);
     })
 
-    // enable the POST form method to sent to mongo
-    app.post("/ingredients/create", async (req,res) => {
-        await db.collection('ingredients').insertOne({
-            'name':req.body.ingredientItem
-        })
+    // create a todo card from user 
+    app.post("/:profile/todo/new", async (req, res) => {
 
-        // after posting, redirect the link to display all ingredients 
-        res.redirect('/ingredients')
     })
 }
 
